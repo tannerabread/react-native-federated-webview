898751a70e33c1e83228df63ff4c2ad62a2d8137
Bannon Tanner <bannonta@amazon.com>
2022-10-26 10:10:07 -0500

    webview working with federated google

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView 
+      allowsBackForwardNavigationGestures
+      source={{ uri: 'http://localhost:3000' }}
+      onNavigationStateChange={(state) => console.log(state)}
+      userAgent={'Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36'}
+    />
   );
 }
 
